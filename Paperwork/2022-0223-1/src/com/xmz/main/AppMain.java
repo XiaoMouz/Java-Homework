ba2d2e6baff5442fea042b9338911486ee0a807d
XiaoMouz <1114096791@qq.com>
2022-02-25 08:51:13 +0800

    Switch Something

--- Paperwork/2022-0223-1/src/com/xmz/main/AppMain.java
+++ Paperwork/2022-0223-1/src/com/xmz/main/AppMain.java
@@ -3,14 +3,13 @@ package com.xmz.main;
 import java.util.HashMap;
 import java.util.Scanner;
 
-import static com.xmz.main.GetInfo.GetUserInfo;
 
 public class AppMain {
     public static void main(String[] args){
         HashMap<String,String> info = new HashMap<String,String>();
         Scanner input = new Scanner(System.in);
         QuickReady.Intialize(info);
-        info = GetUserInfo(info);
+        info = GetInfo.GetUserInfo(info);
 
         for(String i:info.keySet()){
             System.out.println("您的"+i+"是"+info.get(i));
